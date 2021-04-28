840b1e8a4483aa3d93f4585cf74e2e12bdfa1053
Enzo Del Manto <enzombdm@al.insper.edu.br>
2021-04-28 15:27:13 -0300

    extrai naipe

--- paciencia.py
+++ paciencia.py
@@ -1,5 +1,11 @@
-from random import shuffle
+import random
 def cria_baralho():
     baralho = ['A♠', '2♠', '3♠', '4♠', '5♠', '6♠', '7♠', '8♠', '9♠', '10♠', 'J♠', 'Q♠', 'K♠', 'A♥', '2♥', '3♥', '4♥', '5♥', '6♥', '7♥', '8♥', '9♥', '10♥', 'J♥', 'Q♥', 'K♥', 'A♣', '2♣', '3♣', '4♣', '5♣', '6♣', '7♣', '8♣', '9♣', '10♣', 'J♣', 'Q♣', 'K♣', 'A♦', '2♦', '3♦', '4♦', '5♦', '6♦', '7♦', '8♦', '9♦', '10♦', 'J♦', 'Q♦', 'K♦']
-    shuffle(baralho)
+    random.shuffle(baralho)
     return baralho
+
+def extrai_naipe(carta):
+    if len(carta) == 2:
+        return carta[1]
+    else:
+        return carta[2]
